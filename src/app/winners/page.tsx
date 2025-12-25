8e6b2d63f46ef02488ea51ce2336fd0ac50effa9
anh.nguyenduc5 <anh.nguyenduc.work@gmail.com>
2025-12-25 11:20:53 +0700

    Update page.tsx

--- src/app/winners/page.tsx
+++ src/app/winners/page.tsx
@@ -6,8 +6,23 @@ import Link from "next/link";
 
 export default function WinnersPage() {
     return (
-        <div className="min-h-[100dvh] flex flex-col font-sans bg-[#C40405] relative overflow-hidden">
+        <div className="relative min-h-screen flex flex-col font-sans overflow-hidden">
 
+            {/* MOBILE HEADER */}
+            <header
+                className="md:hidden bg-[#b91c1c] text-white px-4 py-2 flex justify-between items-center shadow-lg z-10 relative border-b border-red-900/30 shrink-0 h-[45px]">
+                <div className="flex items-center gap-2">
+                    <Link href="/">
+                        <Home className="w-5 h-5 text-white" />
+                    </Link>
+                    <span className="font-bold text-[10px] uppercase bg-black/20 px-2 py-1 rounded">DANH SÁCH TRÚNG THƯỞNG</span>
+                </div>
+                <div className="flex items-center gap-2">
+                    <button className="p-1 rounded"><MoreHorizontal size={20}/></button>
+                    <button className="p-1 rounded"><X size={20}/></button>
+                </div>
+            </header>
+            
             <div className="hidden xl:block fixed inset-0 z-0 w-full h-full">
                 <Image src="/assets/bg-winner.webp" alt="BG Desktop" fill className="object-fill" priority/>
             </div>
@@ -23,21 +38,6 @@ export default function WinnersPage() {
                 className="object-cover object-top bg-mobile-fill"
               />
             </div>
-
-            {/* MOBILE HEADER */}
-            <header
-                className="md:hidden bg-[#b91c1c] text-white px-4 py-2 flex justify-between items-center shadow-lg z-50 relative border-b border-red-900/30 shrink-0 h-[45px]">
-                <div className="flex items-center gap-2">
-                    <Link href="/">
-                        <Home className="w-5 h-5 text-white" />
-                    </Link>
-                    <span className="font-bold text-[10px] uppercase bg-black/20 px-2 py-1 rounded">DANH SÁCH TRÚNG THƯỞNG</span>
-                </div>
-                <div className="flex items-center gap-2">
-                    <button className="p-1 rounded"><MoreHorizontal size={20}/></button>
-                    <button className="p-1 rounded"><X size={20}/></button>
-                </div>
-            </header>
 
             <main className="relative z-10 flex-1 flex flex-col items-center">
 
