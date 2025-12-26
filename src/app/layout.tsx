e3b6a0ceb5d8c8f7ca1ee84244e6825f22af79ba
anh.nguyenduc5 <anh.nguyenduc.work@gmail.com>
2025-12-26 14:43:31 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,23 @@ export const metadata: Metadata = {
     shortcut: "/favicon-topgia.jpeg",
     apple: "/favicon-topgia.jpeg",
   },
+
+  openGraph: {
+    title: "Rinh Top Gia - Lộc Đầy Nhà",
+    description: "Tham gia ngay để có cơ hội nhận những phần quà vô cùng hấp dẫn cùng Top Gia #rinhtopgia #locdaynha",
+    url: "https://locdaynha.topgiahomecare.vn",
+    siteName: "TopGia",
+    images: [
+      {
+        url: "https://locdaynha.topgiahomecare.vn/og-topgia.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "TopGia preview",
+      },
+    ],
+    locale: "vi_VN",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
